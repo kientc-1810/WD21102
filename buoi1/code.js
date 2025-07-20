4a429135c575f664715f3bd46512ec7a8063f8f5
kientc <kientc@fpt.edu.vn>
2025-07-20 11:21:07 +0700

    code js

---
+++ buoi1/code.js
@@ -0,0 +1,26 @@
+let x = 5;
+{
+let x = 7;
+ console.log(x);
+}
+
+// khai báo thông tin cá nhân của các bạn
+// dùng kiểu chuỗi để khai báo họ và tên, địa chỉ, học vấn
+// dùng kiểu số để khai báo tuổi và năm sinh, điểm gpa
+// dùng kiểu boolean để hiển thị trạng thái học tập (true: đang học, false: nghỉ học)
+
+var hovaten = "Nguyễn Văn A";
+var diachi = "Hà Nội";
+var hocvan = "Thạc sĩ";
+var tuoi = 28;
+var namsinh = 1997;
+var diemGPA = 9.0;
+var trangthai = true;
+
+console.log("Họ và tên: ",hovaten);
+console.log("Địa chỉ: ",diachi);
+console.log("Học vấn: ",hocvan);
+console.log("Tuổi: ",tuoi);
+console.log("Năm sinh: ",namsinh);
+console.log("Trạng thái học: ", trangthai ? "Đang học" : "Nghỉ học");
+
